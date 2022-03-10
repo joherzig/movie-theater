1c170dc81c913764549dc2fec7517be396584b43
Julian Herzig <julian.herzig@itec-products.ch>
2022-03-10 10:10:20 +0100

    extend requests

--- src/middleware/movie.js
+++ src/middleware/movie.js
@@ -5,11 +5,22 @@ const superagent = superagentPromise(_superagent, global.Promise);
 const TOKEN = "Wookie2019";
 
 const request = {
-  get: (url) => superagent.get(url).set("Authorization", `Bearer ${TOKEN}`),
+  get: (url) =>
+    superagent
+      .get(url)
+      .set("Authorization", `Bearer ${TOKEN}`)
+      .then(({ body }) => body)
+      .catch((err) => {
+        console.error(err);
+        return Promise.reject(e);
+      }),
 };
 
 const movieApi = {
-  getList: () => request.get("https://wookie.codesubmit.io/movies"),
+  getList: () =>
+    request.get("https://wookie.codesubmit.io/movies").then((res) => {
+      return res?.movies || [];
+    }),
 };
 
 export default movieApi;
