b5d89d933f4d4698379e36fc42a19f81e756c29b
Julian Herzig <julian.herzig@itec-products.ch>
2022-03-10 11:28:09 +0100

    filter movies

--- src/components/MovieOverview/MovieOverview.js
+++ src/components/MovieOverview/MovieOverview.js
@@ -9,13 +9,19 @@ import "./MovieOverview.css";
 const MovieOverview = () => {
   const [search, setSearch] = useState("");
   const moviesByGenre = useMappedState(movieSelectors.byGenre);
+  const filterMovies = (movies) => {
+    if (!search || !movies?.length) return movies;
+    return movies.filter((movie) => movie.title.toUpperCase().includes(search.toUpperCase()));
+  };
   return (
     <div className="MovieOverview">
       <Appbar search={search} onChange={({ target: { value } }) => setSearch(value)} />
       <div className="MovieOverview-content">
-        {_.map(moviesByGenre, (value, key) => (
-          <Genre key={key} title={key} movies={value} />
-        ))}
+        {_.map(moviesByGenre, (value, key) => {
+          const filteredMovies = filterMovies(value);
+          if (!filteredMovies?.length) return null;
+          return <Genre key={key} title={key} movies={filteredMovies} />;
+        })}
       </div>
     </div>
   );
