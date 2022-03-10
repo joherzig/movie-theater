5251e05d24065b687d393b72ec6f4b11fc11055c
Julian Herzig <julian.herzig@itec-products.ch>
2022-03-10 09:31:39 +0100

    add init

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "redux-react-hook";
 import AppRouter from "../router/AppRouter";
+import { fetchMovieList } from "../store/reducers/movies";
 import "./App.css";
 
-const App = () => (
-  <div className="App">
-    <AppRouter />
-  </div>
-);
+const App = () => {
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    init();
+  });
+
+  const init = async () => {
+    await dispatch(fetchMovieList());
+    setLoaded(true);
+  };
+
+  return <div className="App">{loaded ? <AppRouter /> : <span>Loading...</span>}</div>;
+};
 
 export default App;
